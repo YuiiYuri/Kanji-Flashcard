d410e5cc5df4096bd79b9374a00e697cacddccaf
Vũ Huy Hoàng <115575325+HiraDuong@users.noreply.github.com>
2023-11-29 22:27:54 +0700

    add layout

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require ('express')
+const morgan = require ('morgan')
+const exphbs = require('express-handlebars');
+const path = require('path')
+const app  = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname,'public')))
+// http logger
+app.use(morgan('combined'))
+
+// template 
+const hbs = exphbs.create({ extname: 'hbs' });
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,"resources/views"))
+console.log(path.join(__dirname,"resources/views"))
+
+
+app.get('/',(req,res)=>res.render('home'))
+app.get('/listkanji',(req,res)=>res.render('listkanji'))
+
+app.listen(port,()=> console.log(`Server đang lắng nghe tại http://localhost:${port}`))
